5394fe16c1e3cc93419a4de3643710a1e1776fee
AlexIrungu <aleaxmuiruri@gmail.com>
2023-03-27 19:59:46 +0300

    added methods to power_ controller

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,32 @@
 class PowersController < ApplicationController
+    def index
+        power = Powers.all
+        render json: power, status: :success
+    end
+    
+    def show
+        power = Powers.find_by(id: params[:id])
+        if power
+            render json: power, status: :success
+        else
+            render json: { message: "Power not found" }, status: :not_found
+        end
+    end
+
+    def update 
+        power = Powers.find_by(id: params[:id])
+        if power
+            power.update!(power_params)
+            render json: power, { message: "Updated description" }
+        else
+            render json: { message: "POwer not found" } status: :not_found
+        else
+            render json: { message: "Validation errors" }, status: 404
+        end
+    end
+
+    private
+    def power_params
+        params.permit(:name, :description)
+    end
 end
