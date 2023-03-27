e5e02d1c08d8463d44ccd71ab8cb44b9e145667c
AlexIrungu <aleaxmuiruri@gmail.com>
2023-03-27 20:10:40 +0300

    configured Ability controller

--- app/controllers/abilitys_controller.rb
+++ app/controllers/abilitys_controller.rb
@@ -1,2 +1,14 @@
 class AbilitysController < ApplicationController
+    def create
+        ability = Ability.create!(ability_params)
+        if ability
+            render json: ability, serializer: HeroAndPowerSerializer
+        else
+            render json: { error: "Validation errors" }, status: :not_found
+        end
+    end
+
+    def ability_params
+        params.permit(:strength, :power_id, :hero_id)
+    end
 end
